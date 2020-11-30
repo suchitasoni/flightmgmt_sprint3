cbc4c28c88261821c9f491d62328b09f96d82d69
Suchita <suchita.soni1@gmail.com>
2020-11-30 14:49:02 +0530

    Commit___home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import TabPanel from './TabPanel';
+import { Route, Link, Switch, BrowserRouter, Redirect } from 'react-router-dom';
+import HorizontalLabelPositionBelowStepper from './Stepper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <TabPanel />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Route, Redirect, Switch } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={App} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
